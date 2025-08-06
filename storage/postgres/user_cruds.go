882eadb8c25db70791bbdfb382efa45b2990c6fe
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-08-06 10:21:02 +0500

    feat: interfaces are ready

--- storage/postgres/user_cruds.go
+++ storage/postgres/user_cruds.go
@@ -1,1 +1,15 @@
 package postgres
+
+import (
+	"database/sql"
+
+	"github.com/Ucell/client_manager/storage/repo"
+)
+
+type UserRepository struct {
+	Db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) repo.IUserStorage {
+	return &UserRepository{Db: db}
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"database/sql"
+
+	"github.com/Ucell/client_manager/storage/postgres"
+	"github.com/Ucell/client_manager/storage/repo"
+)
+
+type IStorage interface {
+	User() repo.IUserStorage
+	ClosePDB() error
+}
+
+type databaseStorage struct {
+	pdb *sql.DB
+}
+
+func NewStorage(pdb *sql.DB) IStorage {
+	return &databaseStorage{
+		pdb: pdb,
+	}
+}
+
+func (p *databaseStorage) ClosePDB() error {
+	err := p.pdb.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *databaseStorage) User() repo.IUserStorage {
+	return postgres.NewUserRepository(p.pdb)
+}
