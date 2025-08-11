2003f1c46de72ef121eb37ed7b4eb24fad7d54bf
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-08-11 11:06:16 +0500

    fix: postgres connecton

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -2,22 +2,13 @@ package postgres
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/Ucell/client_manager/configuration"
 	_ "github.com/lib/pq"
 )
 
 func ConnectPdb(conf *configuration.PostgresConfig) (*sql.DB, error) {
-	conDb := fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		conf.Host,
-		conf.Port,
-		conf.User,
-		conf.Name,
-		conf.Password,
-	)
-	db, err := sql.Open("postgres", conDb)
+	db, err := sql.Open("postgres", conf.GetDSN())
 	if err != nil {
 		return nil, err
 	}
